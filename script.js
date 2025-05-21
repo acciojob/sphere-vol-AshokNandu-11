fa457e7e362870b6648262d0d01c9fa2d470d77f
AshokNandu-11 <114278147+AshokNandu-11@users.noreply.github.com>
2025-05-21 09:57:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
+	const radiusInput = document.getElementById("radius");
+      const volumeOutput = document.getElementById("volume");
+      
+      const r = parseFloat(radiusInput.value);
+      
+      if (isNaN(r) || r < 0) {
+        //alert("Please enter a valid positive radius.");
+        volumeOutput.value = "";
+        return;
+      }
+      
+      const volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+      volumeOutput.value = volume.toFixed(2);
+
   
 } 
 
